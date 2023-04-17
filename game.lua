640c86a4bcac237b82a9525a7cfd86bed58b22e4
Lee <lee@kezzi.co>
2023-04-16 20:51:45 -0700

    only generate terrain

--- game.lua
+++ game.lua
@@ -11,7 +11,7 @@ love.filesystem.load("gameplay.lua")()
 love.filesystem.load("inventory.lua")()
 anim8 = require 'anim8/anim8'
 
-view = {zoom = 32, x = 0, y = 0}
+view = {zoom = 16, x = 0.5, y = -60}
 showPerlin = false
 oldMouse = {x = 0, y = 0}
 cursor = {x = 0, y = 0}
@@ -33,45 +33,61 @@ function Game:init()
 
 end
 
-function Game:load()
-  generator = love.thread.newThread("generator.lua")
-  generator:start()
+game_state = {
+  view = { x = 0, y = 0}
+}
 
-  player = Player:new()
+generator_thread = nil
+
+function Game:load()
   terrain = Terrain:new()
   terrain:generateInitial()
-  player.x = 0.5
-  player.y = -60
-  while terrain:getBlock(math.floor(player.y), math.floor(player.x) + 1) == AIR do
-    player.y = player.y + 1
+
+  while terrain:getBlock(math.floor(view.y), math.floor(view.x) + 1) == AIR do
+    view.y = view.y + 1
   end
-  while terrain:getBlock(math.floor(player.y), math.floor(player.x) + 1) ~= AIR do
-    player.y = player.y - 1
+  while terrain:getBlock(math.floor(view.y), math.floor(view.x) + 1) ~= AIR do
+    view.y = view.y - 1
   end
 
-  view.x = player.x
-  view.y = player.y - player.height / 2
-  first = true
+  -- player = Player:new()
+  -- player.x = 0.5
+  -- player.y = -60
+  -- while terrain:getBlock(math.floor(player.y), math.floor(player.x) + 1) == AIR do
+  --   player.y = player.y + 1
+  -- end
+  -- while terrain:getBlock(math.floor(player.y), math.floor(player.x) + 1) ~= AIR do
+  --   player.y = player.y - 1
+  -- end
 
-  player.inventory:give(WOOD_SHOVEL) -- Just temporary, for testing
+  -- view.x = player.x
+  -- view.y = player.y - player.height / 2
+
+  -- player.inventory:give(WOOD_SHOVEL) -- Just temporary, for testing
 end
 
-function Game:activate()
 
+function Game:activate()
+  print("activate")
+  generator_thread = love.thread.newThread("generator.lua")
+  generator_thread:start()
 end
 
 function Game:suspend()
-
+  local command_channel = love.thread.getChannel('generator_command')
+  command_channel:clear()
+  command_channel:push("quit")
+  generator_thread:wait()
 end
 
 
 function Game:update(dt)
-  if dt > 0.1 then dt = 0.1 end
-  local oldx = player.x
-  local oldy = player.y
-  if not first then
-    player:update(dt)
-  end
+  -- if dt > 0.1 then dt = 0.1 end
+  -- local oldx = player.x
+  -- local oldy = player.y
+  -- if not first then
+  --   player:update(dt)
+  -- end
 
   for i = 1, #terrain.entities do
     local entity = terrain.entities[i]
@@ -81,67 +97,68 @@ function Game:update(dt)
     end
   end
 
-  checkCollisions(terrain, player)
+  -- checkCollisions(terrain, player)
 
-  if showInventory then handleInventoryInput(player)
-  else handleGameplayInput(player, terrain, dt)
-  end
+  -- if showInventory then handleInventoryInput(player)
+  -- else handleGameplayInput(player, terrain, dt)
+  -- end
 
-  view.x = view.x + (player.x - view.x) * 0.2
-  view.y = view.y + (player.y - player.height / 2 - view.y) * 0.2
-  local viewDist = pythag(view.x, view.y, player.x, player.y)
-  local maxViewDist = 0.35 * math.min(love.graphics.getWidth(), love.graphics.getHeight()) / view.zoom
-  if viewDist > maxViewDist then
-    view.x = player.x - (player.x - view.x) * (maxViewDist / viewDist)
-    view.y = player.y - (player.y - view.y) * (maxViewDist / viewDist)
-  end
-  first = false
+  -- view.x = view.x + (player.x - view.x) * 0.2
+  -- view.y = view.y + (player.y - player.height / 2 - view.y) * 0.2
+  -- local viewDist = pythag(view.x, view.y, player.x, player.y)
+  -- local maxViewDist = 0.35 * math.min(love.graphics.getWidth(), love.graphics.getHeight()) / view.zoom
+  -- if viewDist > maxViewDist then
+  --   view.x = player.x - (player.x - view.x) * (maxViewDist / viewDist)
+  --   view.y = player.y - (player.y - view.y) * (maxViewDist / viewDist)
+  -- end
+  -- first = false
 
-  placeTime = placeTime + dt
-  player.walk:update(dt)
-  player.hook:update(terrain, dt)
+  -- placeTime = placeTime + dt
+  -- player.walk:update(dt)
+  -- player.hook:update(terrain, dt)
 
   -- Generate new chunks
-  for r = math.floor((player.y - 80) / 32), math.floor((player.y + 80) / 32) do
-    for c = math.floor((player.x - 80) / 32), math.floor((player.x + 80) / 32) do
+  for r = math.floor((view.y - 80) / 32), math.floor((view.y + 80) / 32) do
+    for c = math.floor((view.x - 80) / 32), math.floor((view.x + 80) / 32) do
       terrain:generate(r, c)
     end
   end
+
   terrain:checkGenerator()
 end
 
 
 
 function Game:draw()
-  local x, y = love.mouse.getPosition()
+  -- local x, y = love.mouse.getPosition()
   if showPerlin then terrain:drawPerlin(view)
   else
     terrain:draw(view)
   end
-  love.graphics.setColor(255, 255, 255, 255)
-  player:draw(view)
+  -- love.graphics.setColor(255, 255, 255, 255)
+  -- player:draw(view)
 
-  love.graphics.setColor(0, 0, 0, cursorAlpha)
-  if inreach and not showInventory then
-    love.graphics.setLineWidth(view.zoom/32)
-    love.graphics.setLineStyle("rough")
-    love.graphics.rectangle("line", (math.ceil(cursor.x)-1-view.x)*view.zoom + love.graphics.getWidth()/2, (math.ceil(cursor.y)-1-view.y)*view.zoom+love.graphics.getHeight()/2, view.zoom, view.zoom)
-  end
+  -- love.graphics.setColor(0, 0, 0, cursorAlpha)
+  -- if inreach and not showInventory then
+  --   love.graphics.setLineWidth(view.zoom/32)
+  --   love.graphics.setLineStyle("rough")
+  --   love.graphics.rectangle("line", (math.ceil(cursor.x)-1-view.x)*view.zoom + love.graphics.getWidth()/2, (math.ceil(cursor.y)-1-view.y)*view.zoom+love.graphics.getHeight()/2, view.zoom, view.zoom)
+  -- end
 
   if mineProgress > 0 and mineProgress <= 1 then
     love.graphics.draw(breakImage[math.ceil(mineProgress * 8)], (mineBlock.c-1-view.x)*view.zoom + love.graphics.getWidth()/2, (mineBlock.r-1-view.y)*view.zoom+love.graphics.getHeight()/2, 0, view.zoom/16, view.zoom/16)
   end
 
-  if showInventory then
-    player.inventory:draw()
-  else
-    player.inventory:drawHotbar()
-  end
+  -- if showInventory then
+  --   player.inventory:draw()
+  -- else
+  --   player.inventory:drawHotbar()
+  -- end
 
-  if debug then
-    love.graphics.setColor(0, 0, 0, 255)
-    love.graphics.print(love.timer.getFPS() .. " fps", love.graphics.getWidth() - 150, 50)
-  end
+  -- if debug then
+  --   love.graphics.setColor(0, 0, 0, 255)
+  --   love.graphics.print(love.timer.getFPS() .. " fps", love.graphics.getWidth() - 150, 50)
+  -- end
 end
 
 
