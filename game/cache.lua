fd2e3929ad622b9b5f344d62a87dd9323a0c9359
Lee <lee@kezzi.co>
2023-04-30 19:55:12 -0700

    image cache

--- game/cache.lua
+++ game/cache.lua
@@ -1,5 +1,8 @@
 function Cache()
 	local font_cache = { }
+
+	local image_cache = { }
+	
 	return {
 		font = function(fontname)
 			local key = fontname[1]..','..fontname[2]
@@ -7,12 +10,27 @@ function Cache()
 			local font = font_cache[key]
 
 			if font == nil then
+				print("load font")
 				font = love.graphics.newFont(fontname[1], fontname[2])
 
 				font_cache[key] = font
 			end
 
 			return font
+		end,
+
+		image = function(imagename)
+			local key = imagename
+
+			local image = image_cache[key]
+
+			if image == nil then
+				image = love.graphics.newImage(key, { })
+
+				image_cache[key] = image
+			end
+
+			return image
 		end
 	}
 end
