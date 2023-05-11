c1fb6989b8d124a625b8bf2f8aeb8dc748f68016
Lee <lee@kezzi.co>
2023-05-11 01:23:28 -0700

    movement controls

--- game/game.lua
+++ game/game.lua
@@ -6,16 +6,18 @@ function Game()
   local simulation = nil
 
   local seed_text = nil
-local entity
+
   return {
     state = {
-      view = { 
-      zoom = style.terrain.native_zoom_scale, 
-      x = 0, y = 0,
-      screen_width = 1, screen_height = 1
-    },
-
+      player = Entity(style.entities.black_coffee),
+      
       active = false,
+
+      view = { 
+        zoom = style.terrain.native_zoom_scale, 
+        x = 0, y = 0,
+        screen_width = 1, screen_height = 1
+      }
     },
 
     load = function(self, game_seed, generator_config)
@@ -31,8 +33,7 @@ local entity
 
       simulation = Simulation()
 
-      entity = Entity('mario', Vector.new(80,40))
-      table.insert(simulation.state.entities, entity)
+      table.insert(simulation.state.entities, self.state.player)
     end,
 
     suspend = function(self)
@@ -49,11 +50,35 @@ local entity
       local view = self.state.view
       view.screen_width = global_state.screen_width
       view.screen_height = global_state.screen_height
-      view.x = entity.state.position.x * view.zoom / style.terrain.native_zoom_scale
-      view.y = entity.state.position.y * view.zoom / style.terrain.native_zoom_scale
+
+      local player = self.state.player
+      local p1cs = controllers.player1.state
+
+      if player ~= nil then
+        if self.state.active then
+          view.x = player.state.position.x * view.zoom / style.terrain.native_zoom_scale
+          view.y = player.state.position.y * view.zoom / style.terrain.native_zoom_scale
+        end
+
+        local walk_speed = 10.0
+
+        if p1cs.run > 0.0 then
+          walk_speed = 15.0
+        end
+
+        if p1cs.left > 0.0 then
+          player.state.velocity.x = player.state.velocity.x - p1cs.left * walk_speed
+        end
+
+        if p1cs.right > 0.0 then
+          player.state.velocity.x = player.state.velocity.x + p1cs.right * walk_speed
+        end
+      end
 
       terrain:clean(view)
       terrain:generate(view)
+
+      simulation:clean()
       simulation:update(dt,terrain.state.chunks)
     end,
 
@@ -68,35 +93,29 @@ local entity
         menu:activate()
       end
 
-      delegate.press_left = function()
-        -- self.state.view.x = self.state.view.x - 256 * self.state.view.zoom / style.terrain.native_zoom_scale
-        if entity.state.velocity.x > -1000 then
-          entity.state.velocity.x = entity.state.velocity.x - 100
-        end
+      -- delegate.press_left = function()
+      --   if player.state.velocity.x > -1000 then
+      --     player.state.velocity.x = entity.state.velocity.x - 100
+      --   end
+      -- end
 
-      end
+      -- delegate.press_right = function()
+      --   if entity.state.velocity.x < 1000 then
+      --     entity.state.velocity.x = entity.state.velocity.x + 100
+      --   end
+      -- end
 
-      delegate.press_right = function()
-        -- self.state.view.x = self.state.view.x + 256 * self.state.view.zoom / style.terrain.native_zoom_scale
-        if entity.state.velocity.x < 1000 then
-          entity.state.velocity.x = entity.state.velocity.x + 100
-        end
-      end
+      -- delegate.press_up = function()
+      --   if entity.state.velocity.y > -1000 then
+      --     entity.state.velocity.y = entity.state.velocity.y - 100
+      --   end
+      -- end
 
-      delegate.press_up = function()
-        -- self.state.view.y = self.state.view.y - 256 * self.state.view.zoom / style.terrain.native_zoom_scale
-        if entity.state.velocity.y > -1000 then
-          entity.state.velocity.y = entity.state.velocity.y - 100
-        end
-      end
-
-      delegate.press_down = function()
-        -- self.state.view.y = self.state.view.y + 256 * self.state.view.zoom / style.terrain.native_zoom_scale
-        if entity.state.velocity.y < 1000 then
-          entity.state.velocity.y = entity.state.velocity.y + 100
-        end
-
-      end
+      -- delegate.press_down = function()
+      --   if entity.state.velocity.y < 1000 then
+      --     entity.state.velocity.y = entity.state.velocity.y + 100
+      --   end
+      -- end
 
       delegate.press_function_1 = function()
         local toggle = not terrain.state.render_chunk_borders
@@ -119,7 +138,7 @@ local entity
     end,
 
     suspend = function(self)
-
+      self.state.active = false
     end,
   }
 
